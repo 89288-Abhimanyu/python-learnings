166e86bcc0a3c333c1a96ca3afe636d0ca387c83
Abhimanyu Singh <abhimansing999@gmail.com>
2025-12-31 15:46:57 +0530

    git initialized, .gitignore added, created basic object in python and printed

---
+++ Module1.py
@@ -0,0 +1,8 @@
+name = "Abhimanyu Singh"
+age: int=26
+
+role, department = "Intern", "Development"
+
+print(name, age, role, department)
+
+print(f"Name: {name}, Age: {age}, BornYear: {bornYear}, Role: {role}, Department: {department}")
